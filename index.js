dd4c7116f796450104a68a11f10c9af696f60432
Abhishek Kummar Sharma <89777784+abhidigiworld@users.noreply.github.com>
2026-03-31 20:50:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const port = process.env.PORT || 3000;
 
 const cors = require('cors');
 
-app.use(cors({
-  origin: '*',   // allow all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
